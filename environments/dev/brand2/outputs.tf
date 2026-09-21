d081978f8c841dd3abc63db183b9404d354dcf32
perjoel <169364295+perjoel@users.noreply.github.com>
2026-09-21 09:28:51 +0200

    reset

--- environments/dev/brand2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_id" {
-  description = "Resource ID of the resource group."
-  value = azurerm_resource_group.rg.id
-}
-
-output "resource_group_name" {
-  description = "Name of the resource group."
-  value = azurerm_resource_group.rg.name
-}
